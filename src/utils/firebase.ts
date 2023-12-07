cd25b40fb4d7ed23570990acdeed546a167f2e3c
RyanCFX <94943716+RyanCFX@users.noreply.github.com>
2023-12-06 23:32:48 -0400

    feat: agrega firebase

---
+++ src/utils/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC6UQ4l9Srzaw2361Qw0b_fdTpL5zAuVl0",
+  authDomain: "coopfia.firebaseapp.com",
+  projectId: "coopfia",
+  storageBucket: "coopfia.appspot.com",
+  messagingSenderId: "365120232462",
+  appId: "1:365120232462:web:2f683de1f90099503cf05f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
